166ca356d81b1521f257622fe28e137803fcc12e
issah-ubuntu <nzikustephen826@gmail.com>
2026-01-31 11:31:03 +0300

    - basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // no basePath needed because Apache maps /volta/app/ -> /
+  basePath: "/volta/app",
 };
 
 export default nextConfig;
